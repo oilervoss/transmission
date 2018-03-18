6f35a8152188f3f4e1e9138c53d9f6186aa0e6f8
Euler Alves <euler@alves.pro.br>
2018-03-18 12:06:32 -0300

    Enhancement
    It accepts multiple parameters simultaneously.

--- addtracker.sh
+++ addtracker.sh
@@ -6,57 +6,27 @@
 LIVE_TRACKERS_LIST_CMD='curl -fs --url https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best_ip.txt' 
 TRANSMISSION_REMOTE='/usr/bin/transmission-remote'
 
-PARAMETER="$1"
-[ "$PARAMETER" = "." ] && PARAMETER=" "
 TORRENTS=$($TRANSMISSION_REMOTE -l 2>/dev/null)
 if [ $? -ne 0 ]; then
-    echo -e "\n\e[0;91;1mFail on transmission. Aborting.\n\e[0m"
-    exit 1
+  echo -e "\n\e[0;91;1mFail on transmission. Aborting.\n\e[0m"
+  exit 1
 fi
 
 if [ $# -eq 0 ]; then
-    echo -e "\n\e[31mThis script expects one parameter\e[0m"
+    echo -e "\n\e[31mThis script expects one or more parameters\e[0m"
     echo -e "\e[0;36maddtracker \t\t- list current torrents "
-    echo -e "addtracker \$number\t- add trackers to torrent of number \$number"
-    echo -e "addtracker \$name\t- add trackers to first torrent with part of name \$name"	
+    echo -e "addtracker \$n1 \$n2...\t- add trackers to torrent of number \$n1 and \$n2"
+    echo -e "addtracker \$s1 \$s2...\t- add trackers to first torrent with part of name \$s1 and \$s2"
+    echo -e "addtracker .\t\t- add trackers to all torrents"
+    echo -e "Names are case insensitive "
     echo -e "\n\e[0;32;1mCurrent torrents:\e[0;32m"
     echo "$TORRENTS" | sed -nr 's:(^.{4}).{64}:\1:p'
     echo -e "\n\e[0m"
     exit 1
 fi
 
-if [ ! -z "${PARAMETER//[0-9]}" ] ; then
-	PARAMETER=$(echo "$TORRENTS" | \
-sed -nr '1d;/^Sum:/d;s:(^.{4}).{64}:\1:p' | \
-grep -i "$PARAMETER" | \
-sed -nr 's:(^.{4}).*:\1:;s: ::gp')
-
-	if [ ! -z "$PARAMETER" ] && [ -z ${PARAMETER//[0-9]} ] ; then
-		NUMBERCHECK=1
-		echo -e "\n\e[0;32;1mI found the following torrent:\e[0;32m"
-		echo "$TORRENTS" | sed -nr 's:(^.{4}).{64}:\1:p' | grep -i "$1"
-	else
-		NUMBERCHECK=0
-	fi
-else
-	NUMBERCHECK=$(echo "$TORRENTS" | \
-sed -nr '1d;/^Sum:/d;s: :0:g;s:^(....).*:\1:p' | \
-grep $(echo 0000$PARAMETER | sed -nr 's:.*([0-9]{4}$):\1:p'))
-
-fi
-
-if [ ${NUMBERCHECK:-0} -eq 0 ]; then
-	echo -e "\n\e[0;31;1mI didn't find a torrent with the text/number: \e[21m$1"
-        echo -e "\n\e[0;32;1mCurrent torrents:\e[0;32m"
-	echo "$TORRENTS" | sed -n 's/\(^.\{4\}\).\{64\}/\1/p' 
-	echo -e "\e[0m"
-	exit 1
-fi
-
 TRACKER_LIST=`$LIVE_TRACKERS_LIST_CMD`
-
 if [ $? -ne 0 ] || [ -z "$TRACKER_LIST" ]; then
-
 	TRACKER_LIST="udp://tracker.skyts.net:6969/announce
 udp://tracker.safe.moe:6969/announce
 udp://tracker.piratepublic.com:1337/announce
@@ -80,20 +50,56 @@ udp://tracker.zer0day.to:1337/announce"
 
 fi
 
-for TORRENT in $PARAMETER; do
-	echo -ne "\n\e[0;1;4;32mFor the Torrent: \e[0;4;32m"
-	$TRANSMISSION_REMOTE -t $TORRENT -i | sed -nr 's/ *Name: ?(.*)/\1/p'
-	echo "$TRACKER_LIST" | while read TRACKER
-	do
-		if [ ! -z "$TRACKER" ]; then
-			echo -ne "\e[0;36;1mAdding $TRACKER\e[0;36m"
-			$TRANSMISSION_REMOTE -t $TORRENT -td $TRACKER 1>/dev/null 2>&1 
-			if [ $? -eq 0 ]; then
-				echo -e " -> \e[32mSuccess! "
-			else
-				echo -e " - \e[31m< Failed > "
-			fi
-		fi
-	done
+while [ $# -ne 0 ]; do
+
+  PARAMETER="$1"
+  [ "$PARAMETER" = "." ] && PARAMETER=" "
+
+  if [ ! -z "${PARAMETER//[0-9]}" ] ; then
+    PARAMETER=$(echo "$TORRENTS" | \
+      sed -nr '1d;/^Sum:/d;s:(^.{4}).{64}:\1:p' | \
+      grep -i "$PARAMETER" | \
+      sed -nr 's:(^.{4}).*:\1:;s: ::gp')
+
+    if [ ! -z "$PARAMETER" ] && [ -z ${PARAMETER//[0-9]} ] ; then
+      NUMBERCHECK=1
+      echo -e "\n\e[0;32;1mI found the following torrent:\e[0;32m"
+      echo "$TORRENTS" | sed -nr 's:(^.{4}).{64}:\1:p' | grep -i "$1"
+    else
+      NUMBERCHECK=0
+    fi
+  else
+    NUMBERCHECK=$(echo "$TORRENTS" | \
+      sed -nr '1d;/^Sum:/d;s: :0:g;s:^(....).*:\1:p' | \
+      grep $(echo 0000$PARAMETER | sed -nr 's:.*([0-9]{4}$):\1:p'))
+
+  fi
+
+  if [ ${NUMBERCHECK:-0} -eq 0 ]; then
+    echo -e "\n\e[0;31;1mI didn't find a torrent with the text/number: \e[21m$1"
+    echo -e "\e[0m"
+    shift
+    continue
+  fi
+
+  for TORRENT in $PARAMETER; do
+    echo -ne "\n\e[0;1;4;32mFor the Torrent: \e[0;4;32m"
+    $TRANSMISSION_REMOTE -t $TORRENT -i | sed -nr 's/ *Name: ?(.*)/\1/p'
+    echo "$TRACKER_LIST" | while read TRACKER
+    do
+      if [ ! -z "$TRACKER" ]; then
+        echo -ne "\e[0;36;1mAdding $TRACKER\e[0;36m"
+        $TRANSMISSION_REMOTE -t $TORRENT -td $TRACKER 1>/dev/null 2>&1 
+        if [ $? -eq 0 ]; then
+          echo -e " -> \e[32mSuccess! "
+        else
+          echo -e " - \e[31m< Failed > "
+        fi
+      fi
+    done
+  done
+  
+  shift 
 done
+
 echo -e "\e[0m"
